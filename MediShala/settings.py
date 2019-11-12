31119a18bc91131c8ff58ad3de890cb6a7fb7829
Gourav Sardana <gsardana001@gmail.com>
2019-11-13 00:43:43 +0530

    debug=false

--- MediShala/settings.py
+++ MediShala/settings.py
@@ -23,7 +23,7 @@ location = lambda x: os.path.join(os.path.realpath(BASE_DIR),x)
 SECRET_KEY = '2-&v00j3&#+tq$1m8$e!3fnpr18r-qb3d(r2f-^jorut0%=#%s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
